661260a78327c15337d58fe275808b9b8b8bdbd4
krwi20 <102562042+krwi20@users.noreply.github.com>
2023-03-16 23:31:46 +0000

    Initial commit

---
+++ CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Kris Wittwer on 16/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
